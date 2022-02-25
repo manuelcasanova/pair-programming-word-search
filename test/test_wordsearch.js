c5bdeef2f20e0782f7c94aacf7322cfa2faaf50b
Manuel Casanova <manucasanova@hotmail.com>
2022-02-25 11:55:22 -0800

    PAIRPROGRAMMING WITH SERRA BOTEN AND EDGAR SARGSYAN COMPLETED IN 1 HOUR 45 MINUTES

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -1,7 +1,7 @@
 const chai = require('chai');
 const assert = chai.assert;
 
-const wordSearch = require('../wordsearch.js')
+const wordSearch = require('../wordsearch.js');
 
 describe("#wordSearch()", function() {
   it("should return false if the word is not present", function() {
@@ -15,7 +15,7 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'FRANK')
+    ], 'FRANK');
 
     assert.isFalse(result);
   });
@@ -31,8 +31,50 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'SEINFELD')
+    ], 'SEINFELD');
 
     assert.isTrue(result);
   });
+
+  it("should return a message if the array is empty", function() {
+    const result = wordSearch([], 'SEINFELD');
+      
+    assert.equal(result,"array not valid");
+  });
+
+  it("should return a message if the word is not a string", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 75);
+      
+    assert.equal(result,"word is not a string");
+  });
+
+ 
+  it("should return true if the word is present", function() {
+    const result = wordSearch([
+      ['H', 'E', 'Y', 'C', 'A', 'N', 'A', 'D'],
+      ['E', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['C', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['A', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['N', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['A', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['D', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['A', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'HEYCANAD');
+  
+    assert.isTrue(result);
+  });
+
+
 });
+
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,40 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const transpose = function(matrix) {
+  let results = [];
+  for (let i = 0; i < matrix[0].length; i++) {
+    results.push([]);
+    for (let j = 0; j < matrix.length; j++) {
+      results[i].push(matrix[j][i]);
     }
-}
+  }
+  return results;
+};
 
-module.exports = wordSearch
+const horizontal = function(arr, word) {
+
+  const horizontalJoin = arr.map(ls => ls.join(''));
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+  return false;
+};
+
+const wordSearch = (letters, word) => {
+
+  if (letters.length === 0) {
+    return "array not valid";
+  }
+
+  if (typeof word !== "string") {
+    return "word is not a string";
+  }
+
+  if (horizontal(letters, word) || horizontal(transpose(letters), word)) {
+    return true;
+  } else {
+    return false;
+  }
+
+};
+
+
+module.exports = wordSearch;
